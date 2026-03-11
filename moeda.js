85abe74c8ceb6c0ba971c4517f6e772e628e5f16
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-03-11 15:22:36 -0300

    Add files via upload

---
+++ moeda.js
@@ -0,0 +1,10 @@
+let moedasOuro = 150;
+let qPocao = 4;
+let vPocao = 30;
+
+let restoMoedas = moedasOuro - (qPocao * vPocao);
+
+console.log("O restante das moedas é: " + restoMoedas);
+//NÃO COPIAR - atalho para abrir terminal ctrl + shift + "
+//NÃO COPIAR - OPERADORAS ARITMÉTICOS
+// adição: +, subtração: -, multiplicação: *, divisão: /
