89f4271ba3b229ffded91ac971fb86888f342b31
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-04-14 17:33:24 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,9 @@
+function subirDeNivel(nivelAtual) {
+    return nivelAtual + 1;
+};
+
+let meuNivel = 10;
+let git add = subirDeNivel(meuNivel); // somando nivel atual + 1
+//                   [(nivelAtual + 1) + 10] 
+
+console.log("Seu nível atual é: " + nivelAtualizado);
