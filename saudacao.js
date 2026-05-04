964e249898196690ae28212f13213c0dd4697319
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-05-04 19:19:02 +0000

    versao1

---
+++ saudacao.js
@@ -0,0 +1,8 @@
+let filaEspera = ["Ana", "Bruno", "Carlos"];
+console.log(filaEspera);
+
+filaEspera.push("Diego"); //Adiciona um elemento no final da lista
+console.log(filaEspera);
+
+filaEspera.shift(); //Remove o primeiro elemento da lista
+console.log(filaEspera);
