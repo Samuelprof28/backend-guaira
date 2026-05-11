2eab6bd76fb45bf852b45a7d1550310366bf0d02
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-05-11 19:10:47 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,13 @@
+
+
+
+
+//🔴 Declare 5 variáveis para realizar o cadastro de um aluno (Adrian, Luiza e Erik)
+//🟢 Declare 5 variáveis para um produto de uma loja (Luan e Tiago)
+//🔵 Declare 5 variáveis de um perfil de um jogador (Iago, Ana, Yohann, Débora e Denis)
+//⚫️ Declare 5 variáveis para o funcionário de uma empresa (Leonardo G., Lukas, Luana e Emannuely)
+//e depois imprimir saída interpolada ${}
+
+let variavel = "";
+const numero = ;
+console.log(` ${variavel}`) //Uso de crase
